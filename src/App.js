9eda23935b450da26a109b4a83799788229bb457
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-10 14:13:27 -0600

    setup the basic structure of app for to-do list.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React from "react";
 
+//import todo list and form.
+import TodoList from "./components/TodoComponents/TodoList";
+import TodoForm from "./components/TodoComponents/TodoForm";
+
+//import style for the todo.
+import "./components/TodoComponents/Todo.css";
+
+//setup app structure for to-do list.
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
@@ -7,7 +15,14 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Welcome to your Todo App!</h1>
+
+        <div className="header">
+          <h2>Your Todo</h2>
+          <TodoForm />
+        </div>
+
+        <TodoList />
       </div>
     );
   }
