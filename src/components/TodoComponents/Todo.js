8b8b432f96dcd11c75d25e8f9f8ea0ffb167861f
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-10 15:21:43 -0600

    fixed the line through issue.

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -5,7 +5,7 @@ const Item = props => {
   return (
     <div
       onClick={() => props.toggleItem(props.item.id)}
-      className={`item${props.item.purchased ? " completed" : ""}`}
+      className={`item${props.item.completed ? " completed" : ""}`}
     >
       <p>{props.item.name}</p>
     </div>
